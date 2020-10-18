325ea8e027a599590ddc44b40400ac51424a97c8
leviethung2103 <leviethung1280@gmail.com>
2020-10-18 17:30:48 +0700

    Update install_docker_compose.sh

--- scripts/install_docker_compose.sh
+++ scripts/install_docker_compose.sh
@@ -1,3 +1,11 @@
+# install for windows, linux 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+
+# install for Arm architecture like Raspberry Pi 
+rm -rf /usr/local/bin/docker-compose 
+rm -rf /usr/bin/docker-compose
+pip3 uninstall docker-compose
+pip3 install docker-compose

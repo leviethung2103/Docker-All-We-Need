b54dbcb0411c648213bf2eabb2992113513eced4
leviethung2103 <leviethung1280@gmail.com>
2020-11-25 22:53:29 +0700

    Update install_nvidia_docker.sh

--- scripts/install_nvidia_docker.sh
+++ scripts/install_nvidia_docker.sh
@@ -4,5 +4,6 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo apt-get install -y nvidia-docker2
 sudo systemctl restart docker
-
+sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi

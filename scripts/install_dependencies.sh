3d815b728ce32edc1a46d59d7a9bc73cae225dd3
leviethung2103 <leviethung1280@gmail.com>
2021-06-24 07:28:08 +0700

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+sudo apt-get update -y
+sudo apt-get upgrade
+sudo apt-get install -y gcc python3-dev libatlas-base-dev libv4l-dev libxvidcore-dev libx264-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg-dev libpng-dev libtiff-dev build-essential cmake unzip pkg-config
+sudo apt-get install gfortranlibgtk-3-dev 

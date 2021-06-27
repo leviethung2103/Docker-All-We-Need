3d54941173dcfb8c44905209d2eef388ac2cbccb
leviethung2103 <leviethung1280@gmail.com>
2021-06-27 21:04:03 +0700

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,3 +2,11 @@ sudo apt-get update -y
 sudo apt-get upgrade
 sudo apt-get install -y gcc python3-dev libatlas-base-dev libv4l-dev libxvidcore-dev libx264-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg-dev libpng-dev libtiff-dev build-essential cmake unzip pkg-config
 sudo apt-get install gfortranlibgtk-3-dev 
+sudo apt-get install libncurses5-dev
+
+# Install the NVTOP
+cd ~/Downloads
+git clone https://github.com/Syllo/nvtop.git
+mkdir -p nvtop/build && cd nvtop/build
+cmake ..
+make

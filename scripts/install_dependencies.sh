98361a76cddccf08479d8ad3a33072b38b6d0766
leviethung2103 <leviethung1280@gmail.com>
2021-08-10 21:16:04 +0700

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,11 @@ sudo apt-get install -y gcc python3-dev libatlas-base-dev libv4l-dev libxvidcore
 sudo apt-get install gfortranlibgtk-3-dev 
 sudo apt-get install libncurses5-dev
 
+# Install Docker 
+wget https://raw.githubusercontent.com/leviethung2103/Docker-All-We-Need/master/scripts/install_docker.sh
+chmod +x install_docker.sh
+./install_docker.sh
+
 # Install code-server
 curl -fsSL https://code-server.dev/install.sh | sh
 
